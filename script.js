d6c0537bc9745c3e949567ebc7213e104669d5b9
Wahyu Trisno Aji <trisnoaji070@gmail.com>
2024-12-27 02:31:38 +0700

    feat: add footer and nav (eror lol)

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// humburger
+const humburger = document.querySelector("#humburger");
+
+humburger.addEventListener("click", function () {
+  humburger.classList.toggle("humburger-active");
+});
 // contact section
 window.onbeforeunload = () => {
   for (const form of document.getElementsByTagName("form")) {
