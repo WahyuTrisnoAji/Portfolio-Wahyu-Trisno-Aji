c9864cebcade21f586e9cd1ab6fb4117d6c5d567
Wahyu Trisno Aji <trisnoaji070@gmail.com>
2024-12-27 15:55:27 +0700

    feat: make new navbar

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-// humburger
-const humburger = document.querySelector("#humburger");
+// navbar section
+const hamburgerMenu = document.getElementById("humburger-menu");
+const navbarNavMobile = document.querySelector(".navbar-nav-mobile");
 
-humburger.addEventListener("click", function () {
-  humburger.classList.toggle("humburger-active");
+hamburgerMenu.addEventListener("click", () => {
+  navbarNavMobile.classList.toggle("translate-x-full");
+  navbarNavMobile.classList.toggle("translate-x-0");
 });
+
+document.addEventListener("click", (e) => {
+  if (
+    !hamburgerMenu.contains(e.target) &&
+    !navbarNavMobile.contains(e.target)
+  ) {
+    navbarNavMobile.classList.add("translate-x-full");
+    navbarNavMobile.classList.remove("translate-x-0");
+  }
+});
+
 // contact section
 window.onbeforeunload = () => {
   for (const form of document.getElementsByTagName("form")) {
